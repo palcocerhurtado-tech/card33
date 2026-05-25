d64b4697ab3cbfac664c919cf62dd176a4d813d5
Pablo Alcocer Hurtado <palcocerhurtado@gmail.com>
2026-05-25 08:24:14 +0200

    Update personal data in datos.js

--- datos.js
+++ datos.js
@@ -1,17 +1,17 @@
 var datos = {
-  "nombre": "",
-  "email": "",
-  "telefonoCompleto": "",
-  "telefonoMostrado": "",
-  "empresa": "",
-  "puesto": "",
-  "descripcion": "",
-  "web": "",
-  "ubicacion": "",
-  "portfolioUrl": "",
+  "nombre": "Pablo Alcocer Hurtado",
+  "email": "archon.consultancies@hotmail.com",
+  "telefonoCompleto": "693500740",
+  "telefonoMostrado": "693500740",
+  "empresa": "Archon Consultancies",
+  "puesto": "CEO , IA+Marketing",
+  "descripcion": "Profesionalmultidisciplinar en ia y marketing tecnico ",
+  "web": "https://archonconsultancies.com/",
+  "ubicacion": "zaragoza",
+  "portfolioUrl": "https://palcocerhurtado-tech.github.io/portfolio/",
   "redes": {
-    "linkedin": "",
-    "github": "",
-    "instagram": ""
+    "linkedin": "www.linkedin.com/in/pablo-alcocer-hurtado-a0bb573b6",
+    "github": "https://github.com/palcocerhurtado-tech",
+    "instagram": "https://www.instagram.com/pabloalcoceroficial33/?hl=es"
   }
 };
